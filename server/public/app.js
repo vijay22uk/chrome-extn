b7ce4de30b13e2701a1ba9e1fb74e7bbd36799a6
vijay22uk <vijay22uk@hotmail.com>
2016-12-04 14:04:39 +0530

    showing link to fetched images

--- server/public/app.js
+++ server/public/app.js
@@ -18,37 +18,38 @@
     }
     function renderData(data) {
         /* sample data
-    
-    data
-    :
-    Array[6]
-    origin
-    :
-    "http://stackoverflow.com/questions/26079611/node-js-typeerror-path-must-be-absolute-or-specify-root-to-res-sendfile-failed"
-    readType
-    :
-    "IMG"
-    __proto__
-    :
-    Object
-    <div class="container">
-            <h4>Images</h4>
-            <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">ORIGIN :: www.google.com</button>
-            <div id="demo" class="collapse">
-               
-            </div>
-        </div>
-    */
-     $("#logs").empty();
+        
+        data
+        :
+        Array[6]
+        origin
+        :
+        "http://stackoverflow.com/questions/26079611/node-js-typeerror-path-must-be-absolute-or-specify-root-to-res-sendfile-failed"
+        readType
+        :
+        "IMG"
+        */
+        $("#logs").empty();
         for (var i = 0; i < data.length; i++) {
-            var readType = data[i].readType=="IMG"?"Images":"Flipkart"
+            var readType = data[i].readType == "IMG" ? "Images" : "Flipkart"
             var eleConatiner = $('<div class="container"></div>');
             var h4 = $('<h4 class="bold"><b>' + readType + '</b></h4>');
             eleConatiner.append(h4);
             var btn = $('<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#data' + i + ' "+>ORIGIN :: ' + data[i].origin + '</button>')
             eleConatiner.append(btn);
-            var text = $('<div id="data' + i + '" class="collapse">' + data[i].data.join() + '</div>')
-            eleConatiner.append(text);
+            if (data[i].readType == "IMG") {
+                var collapse = $('<div id="data' + i + '" class="collapse"></div>');
+                for(var j=0;j<data[i].data.length;j++){
+                    var anchor = $('<p><a target="_blank" href="'+ data[i].data[j] +'">'+ data[i].data[j] + '</a></p>');
+                    collapse.append(anchor);
+                }
+                eleConatiner.append(collapse);
+
+            } else {
+                var text = $('<div id="data' + i + '" class="collapse">' + data[i].data.join() + '</div>')
+                eleConatiner.append(text);
+            }
+
             $("#logs").append(eleConatiner)
         }
 
